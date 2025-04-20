d8a9521042bc3d74446978d82fbd703fde44a8f9
Victor <v.delgadoplacido@student.utwente.nl>
2025-04-20 15:17:16 +0200

    like and gettweetfromChat functions added

--- src/lib/server/db_functions.ts
+++ src/lib/server/db_functions.ts
@@ -1,27 +1,43 @@
 import { Database } from 'sqlite'
 
 export interface Tweet {
-    id: number
+    tweet_id: number
     title: string
-    content: string
+    description: string
     likes: number
     created: Date
+    image: string
+    author: string
+    chat_id: number
 }
 
 export interface Chat {
-    id: number
+    chat_id: number
 }
 
-export async function getTweets(db :Database, tweetId: number): Promise<Tweet[]>{
+//Function that extracts array of tweets of a sepcific chat
+export async function getTweetsFromChat(db :Database, chatId: number): Promise<Tweet[]>{
     try {
-        const row = await db.get('SELECT * FROM tweets WHERE id = ?', [tweetId]);
+        const row = await db.all('SELECT * FROM tweets WHERE chat_id = ?', [chatId]);
         return row;
     } catch (err) {
       throw new Error("THIS SHIT AINT WORKING FAM");
     }
 }
 
-export async function getChats(db :Database, userId: number): Promise<Chat[]>{   
+//Function that extracts array of chats of a sepcific user
+export async function getChatsFromUser(db :Database, userId: number): Promise<Chat[]>{   
     const row = await db.all('SELECT * FROM user_chat WHERE user_id = ?', [userId]);
     return row;
 }
+
+export async function LikeTweet(db :Database, tweetId: number){  
+    const row = await db.run('Update tweets Set likes = likes + 1 Where tweet_id = ?', [tweetId]);
+}
+
+
+
+// export async function getTweetIdFromUse(db :Database, userId: number): Promise<Chat[]>{   
+//     const row = await db.all('SELECT * FROM user_chat WHERE user_id = ?', [userId]);
+//     return row;
+// }
